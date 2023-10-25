932332ed3536104bd7fb8dafd09d899d94052730
irvine kwambana <irvenehawks@gmail.com>
2023-10-25 15:52:35 +0200

    user signup and login logic added

--- predictor/forms.py
+++ predictor/forms.py
@@ -1,4 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class SignUpForm(UserCreationForm):
+    #full_name = forms.CharField(max_length=100, help_text='Required. 100 charaters of fewer.')
+
+    class Meta:
+        model = User
+        fields = UserCreationForm.Meta.fields + ('username', 'password',)
 
 class BreastCancerForm(forms.Form):
 
--- predictor/urls.py
+++ predictor/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+
+    #AUTH VIEWS
+    path('', views.landing, name="landing"),
+    path('home/', views.home, name="home"),
+    path('login/', auth_views.LoginView.as_view(),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name="register"),
+
+    # MODEL VIEWS
     path('heart', views.heart, name="heart"),
     path('diabetes', views.diabetes, name="diabetes"),
     path('breast', views.breast, name="breast"),
-    path('', views.home, name="home"),
 ]
